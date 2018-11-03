5883b04af4b0fb6efa2796c2d44ed44274b4c3d2
AshFairburn <ashfairburn@gmail.com>
2018-11-03 19:26:05 +0000

    TROLLFACE

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCam;
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+	}
+
+    void Shoot()
+    {
+      //  RaycastHit hit;
+       // Physics.Raycast();
+    }
+}
