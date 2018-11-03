45fe66ad06315232df972127b525237314de5847
AshFairburn <ashfairburn@gmail.com>
2018-11-03 20:10:48 +0000

    shooting now works

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -1,27 +1,47 @@
 ﻿using UnityEngine;
 
-public class PlayerShoot : MonoBehaviour {
-
+public class PlayerShoot : MonoBehaviour
+{
+    public float firerate = 0.2f;
+    public float time = 0f;
 
     public float damage = 10f;
     public float range = 100f;
 
     public Camera fpsCam;
 
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetButtonDown("Fire1"))
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
         {
-            Shoot();
+            time += Time.deltaTime;
+            if (time >= firerate)
+            {
+                Shoot();
+                time = 0;
+            }
+
         }
-	}
+    }
 
     void Shoot()
     {
         RaycastHit hit;
-        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit))
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit))
         {
             Debug.Log(hit.transform.name);
+            Transform parent = hit.transform.root;
+            if (parent != null)
+            {
+                GameObject target = parent.gameObject;
+                Enemy enemy = target.GetComponent<Enemy>();
+                if (enemy != null)
+                {
+                    enemy.TakeDamage(damage);
+                }
+            }
+
         }
     }
 }
