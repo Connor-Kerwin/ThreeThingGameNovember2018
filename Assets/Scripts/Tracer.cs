12229ec90f6727f98a9a72883f8b3c1ccdb4bc77
Connor Kerwin <connorkerwin@yahoo.co.uk>
2018-11-03 20:24:04 +0000

    Added tracer basis (not implemented yet).

---
+++ Assets/Scripts/Tracer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tracer : MonoBehaviour
+{
+    private TracerPool pool;
+    private Vector3 start;
+    private Vector3 end;
+    private float speed;
+    private float dist;
+    private float length;
+
+    private bool alive;
+
+    public void Link(TracerPool pool)
+    {
+        this.pool = pool;
+    }
+
+    public void Fire(Vector3 start, Vector3 end, float speed)
+    {
+        this.start = start;
+        this.end = end;
+        this.speed = speed;
+
+        alive = true;
+        dist = 0.0f;
+        length = Vector3.Distance(start, end);
+    }
+
+    private void Update()
+    {
+        if (alive) // Is the tracer alive?
+        {
+            dist += Time.deltaTime * speed;
+            if (dist >= length) // Enough time passed to die?
+            {
+                Kill();
+            }
+
+            // Move to position
+            float f = dist / length;
+            Vector3 pos = Vector3.Lerp(start, end, f);
+            transform.position = pos;
+        }
+    }
+
+    private void Kill()
+    {
+        alive = false;
+        pool.KillTracer(this);
+    }
+}
---
+++ Assets/Scripts/TracerPool.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TracerPool
+{
+    private Tracer prefab;
+    private Pool<Tracer> tracers;
+
+    public TracerPool(Tracer prefab)
+    {
+        this.prefab = prefab;
+        tracers = new Pool<Tracer>(Create);
+    }
+
+    public void SpawnTracer(Vector3 start, Vector3 end, float speed)
+    {
+        Tracer tracer = tracers.Get();
+        tracer.Fire(start, end, speed);
+    }
+
+    public void KillTracer(Tracer tracer)
+    {
+        tracers.Store(tracer);
+    }
+
+    private Tracer Create()
+    {
+        Tracer tracer = GameObject.Instantiate<Tracer>(prefab);
+        tracer.Link(this);
+        return tracer;
+    }
+}
