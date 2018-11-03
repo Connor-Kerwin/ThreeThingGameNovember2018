60500b16c9f7b8d756d3e49c0dcab35bda21fd0e
Connor Kerwin <connorkerwin@yahoo.co.uk>
2018-11-03 10:19:10 +0000

    Added some initial folders and stubs for classes.

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    private bool initialized;
+    private static Main instance;
+
+    public bool Initialized { get { return initialized; } }
+    public static Main Instance { get { return instance; } }
+
+    private void Awake()
+    {
+        instance = this;
+
+        if(!PreInitialize())
+        {
+            Debug.LogError("Pre-initialize failed");
+        }
+    }
+
+    private void Start()
+    {
+        if(!Initialize())
+        {
+            Debug.LogError("Initialize failed");
+        }
+    }
+
+    private bool PreInitialize()
+    {
+        return false;
+    }
+
+    private bool Initialize()
+    {
+        return false;
+    }
+
+}
