4ca42c99e7fe197cf36037e474b6fe378fbee21f
AshFairburn <ashfairburn@gmail.com>
2018-11-03 23:43:34 +0000

    health bar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    public Image currentHealthBar;
+    public Text ratioText;
+
+    private float currenthitpoint = 100;
+    private float maxHitPoint = 100;
+
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        Main main = Main.Instance;
+        ManagerStore managerstore = main.ManagerStore;
+        PlayerManager playermanager = managerstore.Get<PlayerManager>();
+        Health hitpoint = playermanager.Health;
+        currenthitpoint = (hitpoint.CurrentHealth / hitpoint.MaxHealth);
+
+        currentHealthBar.rectTransform.localScale = new Vector3(currenthitpoint, 1, 1);
+        ratioText.text = (currenthitpoint * 100).ToString() + "%";
+
+    }
+
+}
