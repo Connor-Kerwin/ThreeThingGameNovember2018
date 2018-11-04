bab80fd187023974352b2bb0ce38b51f192ae243
Connor Kerwin <connorkerwin@yahoo.co.uk>
2018-11-04 13:25:29 +0000

    Added a sanic flip.

---
+++ Assets/Scripts/SanicFlip.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SanicFlip : MonoBehaviour
+{
+    [SerializeField]
+    private float flipRate = 0.1f;
+    private float time;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        time += Time.deltaTime;
+        if(time >= flipRate)
+        {
+            time = 0.0f;
+
+            Vector3 ls = transform.localScale;
+            ls.x *= -1.0f;
+            transform.localScale = ls;
+        }
+	}
+}
