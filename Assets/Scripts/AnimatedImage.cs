df8a1f2e92806926f893e1f07c15ff55740b36a5
Connor Kerwin <connorkerwin@yahoo.co.uk>
2018-11-04 13:23:52 +0000

    Added an animated image for the UI GIFS

---
+++ Assets/Scripts/AnimatedImage.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimatedImage : MonoBehaviour {
+
+    [SerializeField]
+    private Sprite[] sprites;
+    [SerializeField]
+    private Image targetImage;
+    [SerializeField]
+    private float rate = 0.1f;
+
+    float time;
+    int index;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        time += Time.deltaTime;
+        if(time >= rate)
+        {
+            time = 0.0f;
+            Step();
+        }
+	}
+
+    private void Step()
+    {
+        index++;
+        if (index >= sprites.Length)
+        {
+            index = 0;
+        }
+
+        targetImage.sprite = sprites[index];
+    }
+}
