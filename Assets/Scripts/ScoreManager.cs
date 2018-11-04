e307f93656f676d5c93ecbee0449f1f6147badc9
Connor Kerwin <connorkerwin@yahoo.co.uk>
2018-11-04 00:15:55 +0000

    Added a score manager. not yet tested.

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : Manager, IStateMachineListener<GameState>
+{
+    [SerializeField]
+    private int score;
+
+    public int Score { get { return score; } }
+
+    public override bool Link()
+    {
+        Main main = Main.Instance;
+        ManagerStore managerStore = main.ManagerStore;
+        StateManager stateManager = managerStore.Get<StateManager>();
+        stateManager.AddListener(this);
+
+        return base.Link();
+    }
+
+    public void SetScore(int score)
+    {
+        if(ScoreValid(score))
+        {
+            this.score = score;
+        }
+    }
+
+    public void AddScore(int score)
+    {
+        if(ScoreValid(score))
+        {
+            this.score += score;
+        }
+    }
+
+    private bool ScoreValid(int value)
+    {
+        return value > 0;
+    }
+
+    public void OnStateChanged(GameState previous, GameState current)
+    {
+        switch (current)
+        {
+            case GameState.Menu:
+            case GameState.Playing:
+
+                SetScore(0);
+
+                break;
+        }
+    }
+}
